ef5367bc93cab3eeb4bdb6196855dac38de8d276
CecepRusdiana <rusdianac@gmail.com>
2024-06-07 23:50:13 +0700

    react-todo-app

--- react-todo-app/src/components/TodoItem.jsx
+++ react-todo-app/src/components/TodoItem.jsx
@@ -1,15 +1,38 @@
 import React from "react";
+
 const TodoItem = ({ todo }) => {
+  // Definisikan function getTodoTitleStyle di sini
+  const getTodoTitleStyle = () => {
+    if (todo.completed === true) {
+      return { textDecoration: "line-through" };
+    } else {
+      return { textDecoration: "none" };
+    }
+  };
+
   return (
     <div style={styles.todoItem}>
-      <p>{todo.title}</p>
+      {/* Menambahkan checkbox */}
+      <input type="checkbox" style={styles.checkbox} />
+      {/* Panggil function getTodoTitleStyle */}
+      <p style={getTodoTitleStyle()}>{todo.title}</p>
     </div>
   );
 };
+
 const styles = {
   todoItem: {
     border: "2px solid #f4f4f4",
     fontSize: "24px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  checkbox: {
+    marginRight: "10px",
+    height: "18px",
+    width: "18px",
   },
 };
+
 export default TodoItem;
