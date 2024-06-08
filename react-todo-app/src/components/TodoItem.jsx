a677e1cd72972368d094aa85b42ae089d8f42989
rusdianac <83443824+rusdianac@users.noreply.github.com>
2024-06-08 10:32:39 +0700

    Update TodoItem.jsx

--- react-todo-app/src/components/TodoItem.jsx
+++ react-todo-app/src/components/TodoItem.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TodoContext } from "../App";
 
+// Hapus toggleCompleted dan deleteTodo dari props
 const TodoItem = ({ todo }) => {
+  // Peroleh "toggleCompleted" dan "deleteTodo" dari TodoContext
+  const { toggleCompleted, deleteTodo } = useContext(TodoContext);
   // Definisikan function getTodoTitleStyle di sini
   const getTodoTitleStyle = () => {
     if (todo.completed === true) {
@@ -13,9 +17,17 @@ const TodoItem = ({ todo }) => {
   return (
     <div style={styles.todoItem}>
       {/* Menambahkan checkbox */}
-      <input type="checkbox" style={styles.checkbox} />
-      {/* Panggil function getTodoTitleStyle */}
+      <input
+        type="checkbox"
+        style={styles.checkbox}
+        // Jalankan function toggleCompleted ketika checkbox di-klik
+        onChange={() => toggleCompleted(todo.id)} // Memberikan id dari todo sebagai argument
+      />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
+      {/* Tambahkan sebuah button di sini */}
+      <button style={styles.button} onClick={() => deleteTodo(todo.id)}>
+        x
+      </button>
     </div>
   );
 };
@@ -25,13 +37,23 @@ const styles = {
     border: "2px solid #f4f4f4",
     fontSize: "24px",
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
+    padding: "0 20px",
   },
   checkbox: {
-    marginRight: "10px",
     height: "18px",
     width: "18px",
+  },
+  button: {
+    backgroundColor: "#BB0000",
+    color: "#fff",
+    height: "30px",
+    width: "30px",
+    borderRadius: "100%",
+    border: "none",
+    cursor: "pointer",
+    fontSize: "16px",
   },
 };
 
