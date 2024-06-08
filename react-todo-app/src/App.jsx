2e5ebf7c8b60f38b17b04bbdfaadb8605e3cc227
rusdianac <83443824+rusdianac@users.noreply.github.com>
2024-06-08 10:32:14 +0700

    Update App.jsx

--- react-todo-app/src/App.jsx
+++ react-todo-app/src/App.jsx
@@ -1,5 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import Todos from "./components/Todos";
+// Lakukan import TodoForm
+import TodoForm from "./components/TodoForm";
+
+// Buat sebuah context
+export const TodoContext = createContext();
+
 function App() {
   const [todos, setTodos] = useState([
     {
@@ -18,14 +24,53 @@ function App() {
       completed: false,
     },
   ]);
-  console.log(todos);
+
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (todoId) => {
+    // Gunakan filter untuk membuat array baru yang tidak mengandung todo dengan id yang sesuai
+    const updatedTodos = todos.filter((todo) => todo.id !== todoId);
+    // Perbarui state todos dengan array baru
+    setTodos(updatedTodos);
+  };
+
+  // Definisikan function addTodo
+  const addTodo = (todoTitle) => {
+    if (todoTitle === "") {
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    };
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>My Todo List</h1>
-      <Todos todos={todos} />
-    </div>
+    // Bungkus app dengan provider dari context
+    <TodoContext.Provider value={{ toggleCompleted, deleteTodo }}>
+      <div style={styles.container}>
+        <h1 style={styles.title}>My Todo List</h1>
+        {/* Teruskan function addTodo sebagai props */}
+        <TodoForm addTodo={addTodo} />
+        <Todos todos={todos} />
+      </div>
+    </TodoContext.Provider>
   );
 }
+
 const styles = {
   container: {
     textAlign: "center",
